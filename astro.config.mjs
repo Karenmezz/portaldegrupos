7f55ef2463eadcbf8d4c9c9db5c2d464c154c2dc
Karen <karenpimientom@gmail.com>
2025-04-11 15:37:10 -0500

    add changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import configToAlias from '@astropub/config-to-alias/astro'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    configToAlias()
-  ]
+    
 });
